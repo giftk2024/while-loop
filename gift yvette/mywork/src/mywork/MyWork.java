695958a529a7b2f628fa87136b4d77657d510bb6
giftk2024 <giftyvettekomujuni@gmail.com>
2024-10-04 13:19:55 +0300

    Add files via upload

---
+++ gift yvette/mywork/src/mywork/MyWork.java
@@ -0,0 +1,16 @@
+package mywork;
+
+public class MyWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int count = 0;
+	while (count < 5) {
+		System.out.println("Count is: " + count);
+		    count++;
+		// Increment the count to avoid infinite loop
+	}
+	}
+
+}
